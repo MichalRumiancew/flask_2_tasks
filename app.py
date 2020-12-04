105dc97c629da2e1d7511337d2c9e1612b6cdd88
MichalRumiancew <michalr11@gmail.com>
2020-12-04 16:11:31 +0100

    user-stories

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from in_memory_data import user_stories
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return user_stories[0]["title"]
 
 
 if __name__ == '__main__':
---
+++ in_memory_data.py
@@ -0,0 +1,15 @@
+user_stories = [{"id": 0,
+                 "title": "Test Title 1",
+                 "story": "User Story Number One",
+                 "criteria": "This works",
+                 "business": 1000,
+                 "estimation": 4,
+                 "status": "TODO"},
+                {"id": 1,
+                 "title": "Test Title 2",
+                 "story": "User Story Number Two",
+                 "criteria": "This works too",
+                 "business": 5000,
+                 "estimation": 3,
+                 "status": "DONE"}
+                ]
